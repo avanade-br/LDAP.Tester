859c96368872c0be54c4a5cabd9f3d1f58720d67
Samuel Barbosa <samuel.barbosa@avanade.com>
2020-03-13 15:00:23 -0300

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Avanade.Tools.Infrastructure
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtConnection = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbProperty = new System.Windows.Forms.ComboBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(16, 32);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(308, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(16, 88);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(308, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(774, 32);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(89, 34);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "&Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtConnection
+            // 
+            this.txtConnection.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConnection.Location = new System.Drawing.Point(16, 144);
+            this.txtConnection.Name = "txtConnection";
+            this.txtConnection.Size = new System.Drawing.Size(308, 20);
+            this.txtConnection.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "LDAP Server";
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuery.Location = new System.Drawing.Point(350, 32);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(403, 132);
+            this.txtQuery.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(350, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Query";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Property";
+            // 
+            // lstResults
+            // 
+            this.lstResults.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.HorizontalScrollbar = true;
+            this.lstResults.Location = new System.Drawing.Point(16, 200);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(847, 160);
+            this.lstResults.TabIndex = 6;
+            this.lstResults.SelectedIndexChanged += new System.EventHandler(this.lstResults_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(16, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Query Results";
+            // 
+            // cmbProperty
+            // 
+            this.cmbProperty.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProperty.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbProperty.FormattingEnabled = true;
+            this.cmbProperty.Location = new System.Drawing.Point(16, 387);
+            this.cmbProperty.Name = "cmbProperty";
+            this.cmbProperty.Size = new System.Drawing.Size(308, 21);
+            this.cmbProperty.TabIndex = 7;
+            this.cmbProperty.SelectedIndexChanged += new System.EventHandler(this.cmbProperty_SelectedIndexChanged);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue.Location = new System.Drawing.Point(367, 387);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.ReadOnly = true;
+            this.txtValue.Size = new System.Drawing.Size(496, 20);
+            this.txtValue.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(365, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Value";
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(774, 78);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 36);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnRun;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(880, 429);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.cmbProperty);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.txtConnection);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste LDAP";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtConnection;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbProperty;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Avanade.Tools.Infrastructure
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Holds the query results object
+        /// </summary>
+        private SearchResultCollection results;
+
+        /// <summary>
+        /// Form constructor
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Event handler for "Run" button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            const string LDAP_PREFIX = "LDAP://";
+
+            #region User-input Validation
+            if (String.IsNullOrWhiteSpace(txtConnection.Text))
+            {
+                MessageBox.Show($"Please, enter the LDAP server address", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtConnection.Focus();
+                return;
+            }
+            if (String.IsNullOrWhiteSpace(txtUser.Text))
+            {
+                MessageBox.Show($"Please, enter the user name", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUser.Focus();
+                return;
+            }
+            if (String.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show($"Please, enter the user's password", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword.Focus();
+                return;
+            }
+            #endregion
+
+            try
+            {
+                // Change the mouse pointer while the query is being executed
+                this.Cursor = Cursors.WaitCursor;
+
+                // Clear any previous result
+                ClearResults();
+
+                // Prepare the search object
+                var entry = new DirectoryEntry(String.Concat(txtConnection.Text.ToUpper().StartsWith(LDAP_PREFIX) ? "" : LDAP_PREFIX, txtConnection.Text), txtUser.Text, txtPassword.Text);
+                var searcher = new DirectorySearcher(entry)
+                {
+                    PageSize = Byte.MaxValue,
+                    Filter = $"({txtQuery.Text})"
+                };
+
+                // Execute the search in LDAP server
+                results = searcher.FindAll();
+
+                // Verify if there are results
+                if (results == null || results.Count == 0)
+                {
+                    MessageBox.Show($"No results found", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Fill the "Query Results" ListBox
+                BindAndSort(lstResults, results);
+            }
+            catch (LdapException ex)
+            {
+                MessageBox.Show($"Unable to connect to the LDAP server: {ex.ServerErrorMessage ?? ex.Message}", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch(COMException ex)
+            {
+                MessageBox.Show($"Error {ex.ErrorCode}: {ex.Message}", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException ex) when (ex.Source == "System.DirectoryServices")
+            {
+                MessageBox.Show($"Query error: {ex.Message}", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unexpected error: {ex.Message}", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            finally
+            {
+                // Restore the mouse pointer
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        /// <summary>
+        /// Event handler that runs when user clicks on "Exit" buttom
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Event handler that runs when user select an item on "Query Results" ListBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lstResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClearProperties();
+
+            SearchResult obj = this.GetSelectedObject();
+            
+            if (obj == null)
+                return;
+
+            BindAndSort(cmbProperty, obj.Properties.PropertyNames);
+        }
+
+        /// <summary>
+        /// Event handler that runs when user select a property
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbProperty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClearValues();
+
+            SearchResult obj = this.GetSelectedObject();
+
+            if (obj == null)
+                return;
+
+            try
+            {
+                var value = obj.Properties[cmbProperty.Text][0];
+                txtValue.Text = value.ToString();
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Event handler that runs when the applications starts
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Fill the "User Account" TextBox with the current logged user
+            txtUser.Text = String.IsNullOrWhiteSpace(Environment.UserDomainName) ? Environment.UserName : $"{Environment.UserDomainName}\\{Environment.UserName}";
+
+            // Fill the "LDAP Server" TextBox with the address of the domain control, if there is one
+            txtConnection.Text = IPGlobalProperties.GetIPGlobalProperties().DomainName;
+
+            // Define a "default query" that searchs for the current logged user
+            txtQuery.Text = $"samAccountName={Environment.UserName}";
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Return the instance of the LDAP object selected on "Query Results" ListBox
+        /// </summary>
+        /// <returns></returns>
+        private SearchResult GetSelectedObject()
+        {
+            if (results != null && lstResults.SelectedItem != null)
+            {
+                foreach (SearchResult result in results)
+                {
+                    if (lstResults.SelectedItem.Equals(result.Path))
+                        return result;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Load the ListControl with the provided data
+        /// </summary>
+        /// <param name="control">ListControl to be filled</param>
+        /// <param name="data">Data to be loaded</param>
+        private void BindAndSort(ListControl control, IEnumerable data)
+        {
+            var values = new List<string>();
+            foreach (var item in data)
+            {
+                if (item is SearchResult)
+                    values.Add((item as SearchResult).Path);
+                else
+                    values.Add(item.ToString());
+            }
+
+            values.Sort();
+            control.DataSource = values;
+        }
+
+        /// <summary>
+        /// Clear the "Value" TextBox
+        /// </summary>
+        private void ClearValues()
+        {
+            txtValue.Clear();
+            txtValue.Refresh();
+        }
+
+        /// <summary>
+        /// Clear the "Property" ComboBox
+        /// </summary>
+        private void ClearProperties()
+        {
+            ClearValues();
+            cmbProperty.DataSource = null;
+            cmbProperty.Items.Clear();
+            cmbProperty.Refresh();
+        }
+
+        /// <summary>
+        /// Clear the "Query Results" ListBox
+        /// </summary>
+        private void ClearResults()
+        {
+            ClearProperties();
+            lstResults.DataSource = null;
+            lstResults.Items.Clear();
+            results = null;
+        }
+    }
+}
